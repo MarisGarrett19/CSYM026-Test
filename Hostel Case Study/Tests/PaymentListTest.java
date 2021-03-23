a72224642035fade66c49df33a0d4a33ebb3a3ea
proj-jeni <lourdujenifa@gmail.com>
2021-03-23 11:14:51 +0000

    Formatting, naming conventions are done Signed-off-by: proj-jeni <lourdujenifa@gmail.com>

--- Hostel Case Study/Tests/PaymentListTest.java
+++ Hostel Case Study/Tests/PaymentListTest.java
@@ -1,69 +1,51 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PaymentListTest {
 	private PaymentList paymentList;
 	private Payment pay, pay1, pay2;
+
 	@BeforeEach
-	public void setUp() 
-	{
-		paymentList= new PaymentList(4);
-		 pay = new Payment("January",10);
-		 pay1 = new Payment("Febrary",24);
-		 pay2 = new Payment("March",34);
+	public void setUp() {
+		paymentList = new PaymentList(4);
+		pay = new Payment("January", 10);
+		pay1 = new Payment("Febrary", 24);
+		pay2 = new Payment("March", 34);
 		paymentList.add(pay);
 		paymentList.add(pay1);
 		paymentList.add(pay2);
-		
 	}
+	
 	@Test
-	public void testgetPaymentfromPosition() 
-	{
-		
-		Payment position = paymentList.getPayment(1);
-		assertEquals(10,pay.getAmount());
-		
+	public void getPaymentFromPosition() {
+		assertEquals(10, pay.getAmount());
 	}
+	
 	@Test
-	public void testgetPaymentNull() 
-	{
-				
+	public void getPaymentNull() {
 		assertNull(paymentList.getPayment(0));
-			
 	}
 	
 	@Test
-	public void testgetPaymentObject() 
-	{
-	assertNotNull(paymentList.getPayment(2));
+	public void getPaymentObjectNotNull() {
+		assertNotNull(paymentList.getPayment(2));
 	}
 	
 	@Test
-	public void testgetPaymentGreaterthanTotalsize() 
-	{
-			
-		assertNull(paymentList.getPayment(paymentList.getTotal()+1));
-		
-	}
-	
-	
-	@Test
-	public void testgetPaymentTotal() 
-	{
-			
-		assertEquals(68,paymentList.calculateTotalPaid());
-		
+	public void getPaymentGreaterthanTotalsize() {
+		assertNull(paymentList.getPayment(paymentList.getTotal() + 1));
 	}
 	
 	@Test
-	public void testgetPaymentTotalNegative() 
-	{
-			
+	public void getPaymentTotal() {
+		assertEquals(68, paymentList.calculateTotalPaid());
+	}
+	
+	@Test
+	public void getPaymentTotalNegative() {
 		PaymentList newpaymentlist = new PaymentList(1);
-		assertEquals(0,newpaymentlist.calculateTotalPaid());
-		
+		assertEquals(0, newpaymentlist.calculateTotalPaid());
 	}
-	
+
 }
--- Hostel Case Study/Tests/PaymentTest.java
+++ Hostel Case Study/Tests/PaymentTest.java
@@ -1,61 +1,47 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PaymentTest {
 
 	private Payment payment;
-	
+
 	@BeforeEach
-	public void setUp() 
-	{
-	System.out.println("Inside setup");
-	payment = new Payment("January", 100);
-		
+	public void setUp() {
+		System.out.println("Inside setup");
+		payment = new Payment("January", 100);
 	}
-	
+
 	@Test
-	public void testgetMonthfromInput1() 
-	{
+	public void getInputForEquals() {
 		assertEquals("January", payment.getMonth());
-		
 	}
-	
+
 	@Test
-	public void testgetMonthfromInput2() 
-	{
+	public void getInputForNotEquals() {
 		assertNotEquals("February", payment.getMonth());
-		
 	}
-	
+
 	@Test
-	public void testgetMonthNegative() 
-	{
+	public void getMonthValidOrNot() {
 		assertNotEquals("Enter a valid month", payment.getMonth());
-		
 	}
-	
-	
+
 	@Test
-	public void testgetAmountValid() 
-	{
+	public void getAmountEqualsWithGiven() {
 		assertEquals(100, payment.getAmount());
-		
 	}
-	
-	
+
 	@Test
-	public void testgetAmountInValid() 
-	{
+	public void getAmountNotEqualsWithGiven() {
 		assertNotEquals(200, payment.getAmount());
-		
 	}
-	
+
 	@Test
-	public void testgetAmountTrue() 
-	{
+	public void getAmounValidOrNot() {
 		assertNotEquals("Enter a valid amount", payment.getAmount());
-		
 	}
-	
+
 }
